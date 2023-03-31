555637d1cf01e0754f95c3bccd4260450eb7de43
unknown <baoquocnguyenkinh@gmail.com>
2023-03-31 20:27:10 +0700

    Fashion

---
+++ components/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="imgs/email-icon.png" alt="">
+         <h3>email liên hệ</h3>
+         <a href="mailto:quanghuybest@gmail.com">baoquocnguyenkinh@gmail.com</a>
+         <a href="mailto:suppersun1177@gmail.com">anthonynkbq@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="imgs/clock-icon.png" alt="">
+         <h3>giờ mở cửa</h3>
+         <p>Sáng: 07:00 đến 11:00</p>
+         <p>Chiều: 13:00 đến 18:00</p>
+      </div>
+
+      <div class="box">
+         <img src="imgs/map-icon.png" alt="">
+         <h3>địa chỉ</h3>
+         <a href="#">41 Xuân Diệu, Trung Đô, TP VInh, Nghệ An.</a>
+      </div>
+
+      <div class="box">
+         <img src="imgs/phone-icon.png" alt="">
+         <h3>số điện thoại</h3>
+         <a href="tel://0859476166">0984277907</a>
+         <a href="tel://0123456789">0584678077</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Bảo Quốc Pờ Rồ</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="imgs/loading.gif" alt="">
+</div>
